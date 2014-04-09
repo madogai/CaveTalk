bc30b83d6c95a0895f7c31b527a450cff9a5508e
madguy <madguy@drumcan.in>
2014-04-09 17:20:32 +0900

    混雑時に配信開始を押すと落ちてしまう問題を修正

--- CaveTubeClient/CaveTubeEntry.cs
+++ CaveTubeClient/CaveTubeEntry.cs
@@ -3,6 +3,7 @@
 	using System.Collections.Generic;
 	using System.Collections.Specialized;
 	using System.Configuration;
+	using System.IO;
 	using System.Linq;
 	using System.Net;
 	using System.Text;
@@ -14,6 +15,20 @@
 		private static String webUrl = ConfigurationManager.AppSettings["web_server"] ?? "http://gae.cavelis.net";
 		private static String devkey = ConfigurationManager.AppSettings["dev_key"] ?? String.Empty;
 
+		/// <summary>
+		/// 配信開始リクエストを行います。
+		/// </summary>
+		/// <param name="title">タイトル</param>
+		/// <param name="apiKey">APIキー</param>
+		/// <param name="description">配信詳細</param>
+		/// <param name="tags">タグ</param>
+		/// <param name="thumbnailSlot">サムネイルスロット</param>
+		/// <param name="idVisible">ID表示の有無</param>
+		/// <param name="anonymousOnly">ハンドルネーム制限</param>
+		/// <param name="loginOnly">書き込み制限</param>
+		/// <param name="testMode">テストモード</param>
+		/// <param name="socketId">SocketIOの接続ID</param>
+		/// <returns></returns>
 		public static async Task<StartInfo> RequestStartBroadcastAsync(String title, String apiKey, String description, IEnumerable<String> tags, Int32 thumbnailSlot, Boolean idVisible, Boolean anonymousOnly, Boolean loginOnly, Boolean testMode, String socketId) {
 			try {
 				using (var client = new WebClient()) {
@@ -41,11 +56,16 @@
 
 					return new StartInfo(json);
 				}
-			} catch (WebException) {
+			} catch (WebException e) {
 				return null;
 			}
 		}
 
+		/// <summary>
+		/// ユーザー登録情報を取得します。
+		/// </summary>
+		/// <param name="apiKey">APIキー</param>
+		/// <returns></returns>
 		public static async Task<UserData> RequestUserDataAsync(String apiKey) {
 			try {
 				using (var client = new WebClient()) {
@@ -62,6 +82,11 @@
 			}
 		}
 
+		/// <summary>
+		/// ジャンル一覧を取得します。
+		/// </summary>
+		/// <param name="apiKey">APIキー</param>
+		/// <returns></returns>
 		public static async Task<IEnumerable<Genre>> RequestGenre(String apiKey) {
 			try {
 				using (var client = new WebClient()) {
@@ -78,7 +103,7 @@
 			}
 		}
 
-		public class UserData {
+		public sealed class UserData {
 			public IEnumerable<Thumbnail> Thumbnails { get; private set; }
 
 			internal UserData() {
@@ -90,7 +115,7 @@
 			}
 		}
 
-		public class Genre {
+		public sealed class Genre {
 			public String Title { get; private set; }
 			public IEnumerable<String> Tags { get; private set; }
 
@@ -100,7 +125,7 @@
 			}
 		}
 
-		public class Thumbnail {
+		public sealed class Thumbnail {
 			public Int32 Slot { get; private set; }
 			public String Url { get; private set; }
 
@@ -110,7 +135,7 @@
 			}
 		}
 
-		public class StartInfo {
+		public sealed class StartInfo {
 			public String StreamName { get; private set; }
 			public String WarnMessage { get; private set; }
 
